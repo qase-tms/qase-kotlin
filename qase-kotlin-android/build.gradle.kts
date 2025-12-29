9a3c1a80e1c10bb0490947527be44336738be45a
Dmitrii Gridnev <dgridnev@qase.io>
2025-12-29 15:57:16 +0300

    chore(build): set minimum SDK version for Android project
    - Added `minSdk` configuration to `defaultConfig` in `build.gradle.kts`, setting it to 21 for compatibility with Android devices.

--- qase-kotlin-android/build.gradle.kts
+++ qase-kotlin-android/build.gradle.kts
@@ -19,6 +19,10 @@ android {
     namespace = "io.qase.commons.kotlin.android"
     compileSdk = 34
 
+    defaultConfig {
+        minSdk = 21
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
